5bc7c687faa7a3df6f72c4312157b317999772bb
Gokul <gokuldskp@gmail.com>
2020-11-02 20:52:28 +0530

    Add login endpoint

--- api/endpoints/auth.js
+++ api/endpoints/auth.js
@@ -1,51 +1,70 @@
 const db = require('../models');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
+const SALT_ROUNDS = 10;
 const AuthController = {};
 
 AuthController.register = (req, res) => {
 	const { email, full_name, password, join_date } = req.body;
 
-	// Checking if any of the params is missing
-	if(! email || ! full_name || ! password || ! join_date) 
-	{
-		res.status(400).json({
-			"error": "InvalidParams"
-		});
+	if(! email || ! full_name || ! password || ! join_date) {
+		res.status(400).json({"error": "InvalidParams"});
 		return;
 	}
-	
-	db.user.findOrCreate({
-		where: {
-			email
-		},
-		defaults: {
-			email,
-			full_name,
-			password,
-			join_date
-		}
-	}).then(([user, created]) => {
-		// Checking if the user already exists
-		if(! created) {
-			res.status(409).json({
-				"error": "EmailAlreadyExists"
-			});
+
+	bcrypt.hash(password, SALT_ROUNDS, (err, hash) => {
+		if(err) {
+			res.status(400).json({"error": "UnknownError"});
 			return;
 		}
 
-		const access_token = jwt.sign({
-			id: user.id
-		}, process.env.JWT_SECRET);
+		password = hash;
+	});
+	
+	db.user.findOrCreate({
+		where: { email },
+		defaults: { email, full_name, password, join_date }
+	}).then(([user, created]) => {
+		if(! created) {
+			res.status(409).json({"error": "EmailAlreadyExists"});
+			return;
+		}
 
-		res.status(200).json({
-			access_token
-		});
+		const access_token = jwt.sign({id: user.id}, process.env.JWT_SECRET);
+		res.status(200).json({ access_token });
 	}).catch((_) => {
-		// Unknown error occured
 		console.error(_);
-		res.status(400).json({
-			"error": "UnknownError"
+		res.status(400).json({"error": "UnknownError"});
+	});
+}
+
+AuthController.login = (req, res) => {
+	const { email, password } = req.body;
+
+	if(! email || ! password) {
+		res.status(400).json({"error": "InvalidParams"});
+		return;
+	}
+
+	db.user.findOne({where: {email: email}}).then(user => {
+		if(! user) {
+			res.status(403).json({"error": "InvalidEmail"});
+			return;
+		}
+
+		bcrypt.compare(password, user.password, (err, result) => {
+			if(! result || err) {
+				res.status(403).json({"error": "WrongPassword"});
+				return;
+			}
+
+			const access_token = jwt.sign(
+				{id: user.id}, 
+				process.env.JWT_SECRET
+			);
+	
+			res.status(200).json({access_token});
 		});
 	});
 }
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const AuthController = require('../endpoints/auth');
 
 /**
- * @api {post} /auth/register Log In endpoint
- * @apiName Login
+ * @api {post} /auth/register Register
+ * @apiName Register
  * @apiGroup Auth
  * 
  * @apiParam {String} email Email of the user
@@ -12,10 +12,28 @@ const AuthController = require('../endpoints/auth');
  * @apiParam {Date} join_date Date in which the user joined the institution
  * 
  * @apiSuccess (200) {String} access_token
+ * 
  * @apiError EmailAlreadyExists The <code>email</code> already exists
  * @apiError InvalidParmas The params used is invalid
  * @apiError UnknownError Unknown error occured
  */
 router.post('/register', AuthController.register);
+
+/**
+ * @api {post} /auth/login Log In
+ * @apiName Log In
+ * @apiGroup Auth
+ * 
+ * @apiParam {String} email Email of the user
+ * @apiParam {String} password Password of the user
+ * 
+ * @apiSuccess (200) {String} access_token
+ * 
+ * @apiError InvalidParams The params used is invalid
+ * @apiError InvalidEmail The email used does not exist in database
+ * @apiError WrongPassword The email and password does not match
+ * @apiError UnknownError Unknown error occured
+ */
+router.post('/login', AuthController.login);
 
 module.exports = router;
